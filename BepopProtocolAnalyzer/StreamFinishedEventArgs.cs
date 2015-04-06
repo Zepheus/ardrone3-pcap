891bf13cd570f399d4e58520458a6fdaadc374e6
zepheus <vangoethem.c@gmail.com>
2015-04-06 14:37:22 +0200

    Added eventargs

---
+++ BepopProtocolAnalyzer/StreamFinishedEventArgs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BepopProtocolAnalyzer
+{
+    public class StreamFinishedEventArgs : EventArgs
+    {
+        public bool FoundDiscovery { get; private set; }
+
+        public StreamFinishedEventArgs(bool foundDiscovery)
+        {
+            FoundDiscovery = foundDiscovery;
+        }
+    }
+}
